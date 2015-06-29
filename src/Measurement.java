49b6c1f56efe95556de07ea70837a46d85f15fc6
Guy Corlett <guycorlett@gmail.com>
2015-06-29 17:45:25 +0000

    Update to Measurement class

--- src/Measurement.java
+++ src/Measurement.java
@@ -1,20 +1,34 @@
 // This is a class which contains the measurement which will be written to the SQL DB containing all historical temps/humidities
 
-import java.sql;
-
 public class Measurement
 {
     // Hold variables for the measurement
-    private double temperature_local, humidity_local
+    private double temperature_local, humidity_local;
     private long timestamp;
     
     // Initialise the measurement class
     public Measurement(double temperature, double humidity)
     {
-        temperature_local = temperature
-        humidity_local = humidity
-        timestamp = Timestamp.getTime()
+        temperature_local = temperature;
+        humidity_local = humidity;
+        timestamp = System.currentTimeMillis();
     }
     
     // Access temperature
+    public double access_temp(Measurement m)
+    {
+        return m.temperature_local;
+    }
+     
+    // Access humidity
+    public double access_humidity(Measurement m)
+    {
+        return m.humidity_local;
+    }
+    
+    // Access time
+    public long access_time(Measurement m)
+    {
+        return m.timestamp;
+    }
 }
